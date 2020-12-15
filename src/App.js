55f86c88724a7c8062c60595453d3cb45c058162
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-12-15 19:41:33 -0400

    Install Chakra UI and initialized it in the repo.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import "./App.css";
+import React from "react";
+import { Center, Image } from "@chakra-ui/react";
+import logo from "./images/logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello</header>
-    </div>
+    <Center w="100vw" h="100vh" bg="green.600">
+      <Image src={logo} alt="Logo" borderRadius="full" />
+    </Center>
   );
 }
 
